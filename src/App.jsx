95aa5ab51f949804465dd07f1157ee419a0740ae
RonyAbreu <ronyelison78@gmail.com>
2024-03-30 15:10:24 -0300

    Feat: implementando troca de elementos do header de forma dinâmica

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,35 @@ import Footer from "./components/Footer";
 
 // Css
 import "./App.css";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
 
 export const URL_BASE = "http://api.observatorioturismopb.com.br:8085/api/v1";
 
 function App() {
+  const [home, setHome] = useState(false);
+  const [register, setRegister] = useState(false);
+  const [login, setLogin] = useState(false);
+  const [profile, setProfile] = useState(true);
+
+  const path = useLocation().pathname;
+
+  useEffect(() => {
+    if (path === "/theme" || path.startsWith("/user")) {
+      setHome(true);
+      setRegister(true);
+      setLogin(true);
+      setProfile(false);
+    } else {
+      setHome(false);
+      setRegister(false);
+      setLogin(false);
+      setProfile(true);
+    }
+  }, [path]);
 
   return (
     <div className="app">
-      <Header />
+      <Header home={home} register={register} login={login} profile={profile} />
       <Outlet />
       <Footer />
     </div>
