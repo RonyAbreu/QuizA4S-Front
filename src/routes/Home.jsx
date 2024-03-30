a9f7ecb7ea4e4f344f4cd24cbc9286cb78e230bf
RonyAbreu <ronyelison78@gmail.com>
2024-03-30 13:50:42 -0300

    Feat: criação da tela inicial

---
+++ src/routes/Home.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from 'react-router-dom'
+import '../css/Home.css'
+
+const Home = () => {
+
+  const navigate = useNavigate()
+
+  function handleNavigate(){
+    navigate("/theme")
+  }
+
+  return (
+    <div className='home'>
+        <h1 className='home-title'>Aprenda se divertindo</h1>
+        <p className='home-description'>Clique no botão abaixo e inicie um Quiz agora!</p>
+        <button className='home-button' type='button' onClick={handleNavigate}>Jogar</button>
+    </div>
+  )
+}
+
+export default Home
