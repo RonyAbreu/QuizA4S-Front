26588f276197126511d62bbd77e05624614009cc
RonyAbreu <ronyelison78@gmail.com>
2024-03-30 15:07:41 -0300

    Feat: criando o header do site

---
+++ src/components/Header.jsx
@@ -0,0 +1,57 @@
+import { Link, NavLink } from "react-router-dom";
+import logo from "../assets/logo-a4s.png";
+
+import "../css/Header.css";
+
+const Header = ({ home, register, login, profile }) => {
+  return (
+    <header className="header">
+      <Link to="/">
+        <img src={logo} alt="Logo Apps4Society" className="logo" />
+      </Link>
+
+      <h1 className="title">Quiz A4S</h1>
+
+      <ul className="nav-bar">
+        {!home && (
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "nav-item active" : "nav-item"
+            }
+          >
+            Início
+          </NavLink>
+        )}
+
+        {!register && (
+          <NavLink
+            to="/register"
+            className={({ isActive }) =>
+              isActive ? "nav-item active" : "nav-item"
+            }
+          >
+            Cadastrar-se
+          </NavLink>
+        )}
+
+        {!login && (
+          <NavLink
+            to="/login"
+            className={({ isActive }) =>
+              isActive ? "nav-item active" : "nav-item"
+            }
+          >
+            Login
+          </NavLink>
+        )}
+
+        {!profile && (
+          <Link className="profile">Profile</Link>
+        )}
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
