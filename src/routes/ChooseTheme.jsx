1019b49ad7f87d5a648fc958ef20ccac934d82f1
RonyAbreu <ronyelison78@gmail.com>
2024-03-31 00:33:56 -0300

    Feat: buscando todos os temas, e temas pelo nome

--- src/routes/ChooseTheme.jsx
+++ src/routes/ChooseTheme.jsx
@@ -1,11 +1,78 @@
-import React from 'react'
+// Components
+import { useEffect, useState } from "react";
+import { URL_BASE } from "../App";
+
+//Css
+import "../css/ChooseTheme.css";
+
+const url = `${URL_BASE}/theme`;
 
 const ChooseTheme = () => {
-  return (
-    <div className='container-theme'>
-      <h1>Escolha um tema</h1>
-    </div>
-  )
-}
+  const [themeName, setThemeName] = useState("");
+  const [themes, setThemes] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-export default ChooseTheme
+  async function searchThemeName(e) {
+    const inputName = e.target.value;
+
+    setThemeName(inputName);
+
+    setLoading(true);
+    const response = await fetch(`${url}/search?name=${inputName}`);
+
+    const pageOfThemesByName = await response.json();
+    const themesByName = pageOfThemesByName.content;
+    setLoading(false);
+
+    setThemes(themesByName);
+  }
+
+  useEffect(() => {
+    async function getAllThemes() {
+      setLoading(true);
+      const response = await fetch(url);
+
+      const page = await response.json();
+      const themes = page.content;
+      setLoading(false);
+      setThemes(themes);
+    }
+
+    getAllThemes();
+  }, []);
+
+  return (
+    <div className="container-theme">
+      <div className="container-theme-data">
+        <div className="theme-form">
+          <h1>Escolha o tema do seu Quiz</h1>
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o nome de um tema"
+              value={themeName}
+              onChange={searchThemeName}
+            />
+          </form>
+        </div>
+
+        <div className="container-all-themes">
+          {themes &&
+            themes.map((theme) => (
+              <div className="theme" key={theme.id}>
+                <img src={theme.imageUrl} alt="theme-image" />
+                <p>{theme.name}</p>
+              </div>
+            ))}
+
+          {!themes && <h2 className="not-found">Nenhum tema encontrado!</h2>}
+          <div className="container-info">
+            {loading && <h2 className="loading">Carregando...</h2>}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChooseTheme;
