7852705507a2bb1500c45b1ecc494aa4e32c9696
RonyAbreu <ronyelison78@gmail.com>
2024-04-03 17:52:48 -0300

    Feat: implementando a lógica do quiz

--- src/routes/Quiz.jsx
+++ src/routes/Quiz.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { URL_BASE } from "../App";
 import Question from "../components/Question";
 
@@ -10,6 +10,10 @@ const Quiz = () => {
   const path = useLocation().pathname;
   const [questions, setQuestions] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const navigate = useNavigate()
 
   let themeId;
 
@@ -34,10 +38,36 @@ const Quiz = () => {
     getQuestionsByThemeId();
   }, []);
 
+  function handleAnswerClick(alternative) {
+    if (currentQuestionIndex === questions.length - 1) {
+      alert("quiz acabou");
+      alert(`Você acertou ${score + 1} de 10 questões!`);
+      navigate("/theme")
+      return;
+    }
+
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+
+    if (isAlternativeCorrect(alternative)) {
+      setScore(score + 1);
+    }
+  }
+
+  function isAlternativeCorrect(alternative){
+    return alternative.target.getAttribute("value") === "true";
+  }
 
   return (
-    <div className="container-quiz outlet">
+    <div className="container-quiz">
       {loading && <h1>Carregando...</h1>}
+
+      {questions.length > 0 && (
+        <Question
+          title={questions[currentQuestionIndex].title}
+          alternatives={questions[currentQuestionIndex].alternatives}
+          onAnswerClick={handleAnswerClick}
+        />
+      )}
     </div>
   );
 };
