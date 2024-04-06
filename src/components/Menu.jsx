0839f37c214c7cc32b19f7f5df528df5bb3c8408
RonyAbreu <ronyelison78@gmail.com>
2024-04-05 23:56:45 -0300

    Feat: criação do componente menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,10 @@
+const Menu = () => {
+  return (
+    <div>
+        <button type="button">Meu Perfil</button>
+        <button type="button">Criar Quiz</button>
+    </div>
+  )
+}
+
+export default Menu
