02a657bf24d75ab8e90d699f52f89ded5e653ef2
RonyAbreu <ronyelison78@gmail.com>
2024-04-22 12:28:43 -0300

    Feat: adicionando funcionalidade de remover e atualizar questão

--- src/routes/myquestion/MyQuestion.jsx
+++ src/routes/myquestion/MyQuestion.jsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from "react";
 import Pagination from "../../components/pagination/Pagination";
 import SearchComponent from "../../components/searchComponent/SearchComponent";
 import { ApiFetch } from "../../util/ApiFetch";
+import Loading from "../../components/loading/Loading";
+import InformationBox from "../../components/informationBox/InformationBox";
+import ConfirmBox from "../../components/confirmBox/ConfirmBox";
+import UpdateBox from "../../components/updateBox/UpdateBox";
+
 import "./MyQuestion.css";
 
 const defaultImgUrl =
@@ -22,6 +27,19 @@ const MyQuestion = () => {
 
   const basePath = `/question/creator/theme/${themeId}?page=${currentPage}&title=`;
 
+  const [loading, setLoading] = useState(false);
+  const [isConfirmBox, setConfirmBox] = useState(false);
+  const [isUpdateBox, setUpdateBox] = useState(false);
+  const [isInformationBox, setInformationBox] = useState(false);
+
+  const [callBack, setCallBack] = useState();
+
+  const [informationData, setInformationData] = useState({
+    text: "",
+    icon: "exclamation",
+    color: "red",
+  });
+
   useEffect(() => {
     const promisse = apiFetch.getPagesWithToken(
       basePath,
@@ -36,7 +54,105 @@ const MyQuestion = () => {
       setTotalPages(response.totalPages);
       setQuestions(response.data);
     });
-  }, [currentPage]);
+  }, [currentPage, callBack]);
+
+  const [newQuestion, setNewQuestion] = useState({
+    title: "",
+    imageUrl: "",
+  });
+
+  const inputs = [
+    {
+      label: "Novo título",
+      type: "text",
+      placeholder: "Digite o título da questão",
+      value: newQuestion.title,
+    },
+    {
+      label: "URL da Imagem",
+      type: "text",
+      placeholder: "Digite a url da imagem",
+      value: newQuestion.imageUrl,
+    },
+  ];
+
+  function changeValue(value, label) {
+    switch (label) {
+      case "Novo título":
+        setNewQuestion({ ...newQuestion, title: value });
+        return;
+      case "URL da Imagem":
+        setNewQuestion({ ...newQuestion, imageUrl: value });
+        return;
+      default:
+        return "";
+    }
+  }
+
+  const [questionId, setQuestionId] = useState(0);
+
+  function showConfirmBox(id, title, imageUrl) {
+    setQuestionId(id);
+    setNewQuestion({title: title, imageUrl: imageUrl})
+    setConfirmBox(true)
+  }
+
+  function showUpdateBox(id, title, imageUrl) {
+    setQuestionId(id);
+    setNewQuestion({title: title, imageUrl: imageUrl})
+    setUpdateBox(true)
+  }
+
+  function removeQuestion(){
+    setLoading(true);
+    const promisse = apiFetch.delete(`/question/${questionId}`, false);
+
+    promisse.then((response) => {
+      if (!response.removed) {
+        activeInformationBox(true, response.message);
+        setLoading(false);
+        return;
+      }
+
+      activeInformationBox(false, "Questão removida com sucesso!");
+      setQuestions(questions.filter((quest) => questionId !== quest.id));
+      setLoading(false);
+      setConfirmBox(false);
+    });
+  }
+
+  function updateQuestion(){
+    setLoading(true);
+    const promisse = apiFetch.patch(`/question/${questionId}`, newQuestion);
+
+    promisse.then((response) => {
+      if (!response.success) {
+        activeInformationBox(true, response.message);
+        setLoading(false);
+        return;
+      }
+
+      activeInformationBox(false, "Questão atualizada com sucesso");
+      setCallBack({});
+      setLoading(false);
+      setUpdateBox(false);
+    });
+    
+  }
+
+  function activeInformationBox(isFail, message) {
+    if (isFail) {
+      setInformationData((prevData) => {
+        return { ...prevData, text: message };
+      });
+      setInformationBox(true);
+    } else {
+      setInformationData((prevData) => {
+        return { ...prevData, text: message, color: "green", icon: "check" };
+      });
+      setInformationBox(true);
+    }
+  }
 
   return (
     <div className="container-my-question outlet">
@@ -76,8 +192,26 @@ const MyQuestion = () => {
                   </div>
 
                   <div className="question-action">
-                    <i className="bi bi-trash-fill"></i>
-                    <i className="bi bi-pencil-square"></i>
+                    <i
+                      className="bi bi-trash-fill"
+                      onClick={() =>
+                        showConfirmBox(
+                          question.id,
+                          question.title,
+                          question.imageUrl
+                        )
+                      }
+                    ></i>
+                    <i
+                      className="bi bi-pencil-square"
+                      onClick={() =>
+                        showUpdateBox(
+                          question.id,
+                          question.title,
+                          question.imageUrl
+                        )
+                      }
+                    ></i>
                   </div>
                 </div>
               ))}
@@ -90,6 +224,35 @@ const MyQuestion = () => {
           />
         </div>
       </div>
+
+      {isConfirmBox && (
+        <ConfirmBox
+          title="Deseja remover esta questão?"
+          textBtn1="Sim"
+          textBtn2="Não"
+          onClickBtn1={removeQuestion}
+          onClickBtn2={() => setConfirmBox(false)}
+        />
+      )}
+      {isUpdateBox && (
+        <UpdateBox
+          title="Atualizar Tema"
+          inputs={inputs}
+          onChange={changeValue}
+          onClickSave={updateQuestion}
+          onClickCancel={() => setUpdateBox(false)}
+        />
+      )}
+      {isInformationBox && (
+        <InformationBox
+          text={informationData.text}
+          color={informationData.color}
+          icon={informationData.icon}
+          closeBox={() => setInformationBox(false)}
+        />
+      )}
+
+      {loading && <Loading />}
     </div>
   );
 };
