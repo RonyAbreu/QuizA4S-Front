9fc783f9ec4d8ee6e735aa80299d76512b43e7ef
RonyAbreu <ronyelison78@gmail.com>
2024-04-26 20:15:35 -0300

    Refactor: adicionando rota secundária para página de criar quiz

--- src/main.jsx
+++ src/main.jsx
@@ -30,6 +30,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             <Route path="/theme" element={<ChooseTheme />}/>
             <Route path="/theme/quiz/:id" element={<Quiz />}/>
             <Route path="/create/quiz" element={<CreateQuiz />}/>
+            <Route path="/create/quiz/:id" element={<CreateQuiz />}/>
             <Route path="/profile" element={<Profile />}/>
             <Route path="/profile/theme/:id/question" element={<MyQuestion />}/>
             <Route path="*" element={<ErrorPage />} />
