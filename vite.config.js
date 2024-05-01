c9a930000cebbb53d3c3d56a0fc99cc77ebfbbcc
RonyAbreu <ronyelison78@gmail.com>
2024-04-30 21:32:11 -0300

    Build: alterando configuração de build do projeto

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  preview: {
+   port: 80,
+   strictPort: true,
+   host: true,
+  },
+ });
